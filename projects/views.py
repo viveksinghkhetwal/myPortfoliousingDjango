05c3a6a577a8515f653211ce31a78908b8f54dcb
viveksinghkhetwal <48482586+viveksinghkhetwal@users.noreply.github.com>
2021-04-23 17:36:51 +0530

    first commit

---
+++ projects/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Project
+
+# Create your views here.
+def home(request):
+    projects = Project.objects
+    return render(request, 'projects/home.html', {'projects':projects})
